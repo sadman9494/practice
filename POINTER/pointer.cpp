cae92b48b9dcf21d31389b3e73f07b5147da8a54
Saqib <20-43435-1@student.aiub.edu>
2023-06-01 00:56:35 +0600

    pointer

--- POINTER/pointer.cpp
+++ POINTER/pointer.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    // declaring a pointer 
-    int* ptr; 
-    // declaring a variable
-    int b;
-    //
-    int a;
-    ptr = &a;
 
-    cout << ptr<<endl;
-    cout << *ptr <<endl;
-    cout << &ptr<< endl;
-    
+int main() {
+    int* ptr; // declaring a pointer
+    int b;    // declaring a variable
+    int a;    // declaring another variable
 
-    cout << a <<endl;
-    cout << &a<<endl;
-   // cout<< &b<<endl;
+    ptr = &a; // assigning the address of 'a' to 'ptr'
 
+    cout << ptr << endl;   // Output: Address of 'a' (e.g., 0x61fe10)
+    cout << *ptr << endl;  // Output: Value stored at the address pointed by 'ptr' (e.g., 14835856)
+    cout << &ptr << endl;  // Output: Address of 'ptr' itself (e.g., 0x61fe18)
+
+    cout << a << endl;     // Output: Value of 'a' (e.g., 14835856)
+    cout << &a << endl;    // Output: Address of 'a' (e.g., 0x61fe10)
+    cout << &b << endl;    // Output: Address of 'b' (e.g., 0x61fe14)
+
+    return 0;
 }
