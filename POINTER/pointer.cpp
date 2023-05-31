68010ef99fed1aaaf766705f9d48c568efd2ccb6
Saqib <20-43435-1@student.aiub.edu>
2023-05-31 23:36:47 +0600

    pointer

---
+++ POINTER/pointer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // declaring a pointer 
+    int* ptr; 
+    // declaring a variable
+    int b;
+    //
+    int a;
+    ptr = &a;
+
+    cout << ptr<<endl;
+    cout << *ptr <<endl;
+    cout << &ptr<< endl;
+    
+
+    cout << a <<endl;
+    cout << &a<<endl;
+   // cout<< &b<<endl;
+
+}
